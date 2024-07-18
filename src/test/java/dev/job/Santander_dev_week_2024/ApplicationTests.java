a5edb171fef6e2d4ec78b3e84309221c8b9a06b5
Lucas Job Viana <lucasjobvianna@gmail.com>
2024-07-18 19:43:06 -0300

    :tada: Iniciando projeto.

---
+++ src/test/java/dev/job/Santander_dev_week_2024/ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.job.Santander_dev_week_2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
