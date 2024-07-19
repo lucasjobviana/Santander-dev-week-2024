8b2edc71ff1781e3f3fe8588c9f13014fb6a7564
Lucas Job Viana <lucasjobvianna@gmail.com>
2024-07-18 23:15:47 -0300

    :bug: fix: Alterado double para BigDecimal e adicionado precisão numérica.

--- src/main/java/dev/job/Santander_dev_week_2024/domain/model/Account.java
+++ src/main/java/dev/job/Santander_dev_week_2024/domain/model/Account.java
@@ -3,15 +3,17 @@ package dev.job.Santander_dev_week_2024.domain.model;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 
+import java.math.BigDecimal;
+
 @Entity(name = "account")
 public class Account extends ModelObject{
     @Column(unique = true)
     private String number;
     private String agency;
-    @Column(precision = 2)
-    private double balance;
-    @Column(name = "additional_limit", precision = 2)
-    private double limit;
+    @Column(precision = 13, scale = 2)
+    private BigDecimal balance;
+    @Column(name = "additional_limit", precision = 13, scale = 2)
+    private BigDecimal limit;
 
     public String getNumber() {
         return number;
@@ -29,19 +31,19 @@ public class Account extends ModelObject{
         this.agency = agency;
     }
 
-    public double getBalance() {
+    public BigDecimal getBalance() {
         return balance;
     }
 
-    public void setBalance(double balance) {
+    public void setBalance(BigDecimal balance) {
         this.balance = balance;
     }
 
-    public double getLimit() {
+    public BigDecimal getLimit() {
         return limit;
     }
 
-    public void setLimit(double limit) {
+    public void setLimit(BigDecimal limit) {
         this.limit = limit;
     }
 }
--- src/main/java/dev/job/Santander_dev_week_2024/domain/model/Card.java
+++ src/main/java/dev/job/Santander_dev_week_2024/domain/model/Card.java
@@ -3,12 +3,14 @@ package dev.job.Santander_dev_week_2024.domain.model;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 
+import java.math.BigDecimal;
+
 @Entity(name = "card")
 public class Card extends ModelObject{
     @Column(unique = true)
     private String number;
-    @Column(name = "available_limit", precision = 2)
-    private double limit;
+    @Column(name = "available_limit", precision = 13, scale = 2)
+    private BigDecimal limit;
 
     public String getNumber() {
         return number;
@@ -18,11 +20,11 @@ public class Card extends ModelObject{
         this.number = number;
     }
 
-    public double getLimit() {
+    public BigDecimal getLimit() {
         return limit;
     }
 
-    public void setLimit(double limit) {
+    public void setLimit(BigDecimal limit) {
         this.limit = limit;
     }
 }
